79fdb34f1063115687aa352436c3a85714682cb5
Raaghu1987 <raghavendra@icircle.co.in>
2015-03-16 11:11:53 +0530

    commit 1

---
+++ src/DocxTemplate.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Raaghu
+ * Date: 16/3/15
+ * Time: 10:05 AM
+ */
+
+class DocxTemplate {
+    private $template = null;
+
+    function __construct($templatePath){
+        if(!file_exists($templatePath)){
+            throw new Exception("Invalid Template Path");
+        }
+        $this->template = $templatePath;
+    }
+
+    function merge($data){
+        //open the Archieve to a temp folder
+
+        $workingFile = tempnam(sys_get_temp_dir(),'');
+        if($workingFile === FALSE || !copy($this->template,$workingFile)){
+            throw new Exception("Error in initializing working copy of the template");
+        }
+
+
+
+
+    }
+
+
+
+} 
